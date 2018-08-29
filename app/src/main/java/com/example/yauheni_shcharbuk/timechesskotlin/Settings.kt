69224f2716b5dba0c8d51d07b5927a57969ceae6
Yauheni_Shcharbuk <evgenus196@gmail.com>
2018-08-29 16:18:04 +0300

    finish version without add time

--- app/src/main/java/com/example/yauheni_shcharbuk/timechesskotlin/Settings.kt
+++ app/src/main/java/com/example/yauheni_shcharbuk/timechesskotlin/Settings.kt
@@ -6,8 +6,7 @@ import android.preference.Preference
 import android.preference.PreferenceActivity
 import android.preference.PreferenceFragment
 
-class Settings : PreferenceActivity() {
-
+class Settings : PreferenceActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,14 +18,24 @@ class Settings : PreferenceActivity() {
             super.onCreate(savedInstanceState)
             addPreferencesFromResource(R.xml.preference)
 
-            val checkBoxPreference: CheckBoxPreference = findPreference(getString(R.string.key_add_time)) as CheckBoxPreference
-            val numberPickerPreference = findPreference(getString(R.string.key_add_time_change))
-            numberPickerPreference.isEnabled = checkBoxPreference.isChecked
+            val sp = preferenceScreen.sharedPreferences
+            val time = findPreference(getString(R.string.key_time))
 
-            checkBoxPreference.onPreferenceClickListener = Preference.OnPreferenceClickListener {
-                numberPickerPreference.isEnabled = checkBoxPreference.isChecked
-                false
+            time.summary = sp.getInt(getString(R.string.key_time), Constants.DEFAULT_TIME.toInt() / 60).toString() + " минут"
+
+            time.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { p0, p1 ->
+                time.summary = p1.toString() + " минут"
+                true
             }
+
+//            val checkBoxPreference: CheckBoxPreference = findPreference(getString(R.string.key_add_time)) as CheckBoxPreference
+//            val numberPickerPreference = findPreference(getString(R.string.key_add_time_change))
+//            numberPickerPreference.isEnabled = checkBoxPreference.isChecked
+//
+//            checkBoxPreference.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+//                numberPickerPreference.isEnabled = checkBoxPreference.isChecked
+//                false
+//            }
         }
     }
 }
